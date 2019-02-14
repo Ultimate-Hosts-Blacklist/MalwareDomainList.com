343762978b3f46fb6c2c32cb9e37f9efc14a3e03
Travis <dead-hosts@funilrys.com>
2019-02-14 14:27:18 +0000

    [Autosave] Testing for Ultimate Hosts Blacklist

--- administration.py
+++ administration.py
@@ -15,7 +15,8 @@ Contributors:
     @GitHubUsername, Name, Email (optional)
 """
 from update import Helpers, Settings, path, strftime
-from whitelisting import Whitelist
+from ultimate_hosts_blacklist_the_whitelist import clean_list_with_official_whitelist
+from PyFunceble import syntax_check
 
 INFO = {}
 
@@ -37,7 +38,8 @@ def update_adminisation_file():
     Update what should be updated.
     """
 
-    INFO.update({"currently_under_test": str(int(False)), "last_test": strftime("%s")})
+    INFO.update({"currently_under_test": str(
+        int(False)), "last_test": strftime("%s")})
 
 
 def save_administration_file():
@@ -54,28 +56,40 @@ def generate_clean_and_whitelisted_list():
     """
 
     if bool(int(INFO["clean_original"])):
-        clean_list = []
+        clean_list = temp_clean_list = []
 
         list_special_content = Helpers.Regex(
-            Helpers.File(Settings.file_to_test + Settings.list_name).to_list(), r"ALL\s"
+            Helpers.File(Settings.file_to_test).to_list(), r"ALL\s"
         ).matching_list()
 
         active = Settings.current_directory + "output/domains/ACTIVE/list"
 
         if path.isfile(active):
-            clean_list.extend(
-                Helpers.Regex(Helpers.File(active).to_list(), r"^#").not_matching_list()
+            temp_clean_list.extend(
+                Helpers.Regex(Helpers.File(active).to_list(),
+                              r"^#").not_matching_list()
                 + list_special_content
             )
 
+        temp_clean_list = Helpers.List(temp_clean_list).format()
+
+        for element in temp_clean_list:
+            if element:
+                if syntax_check(element):
+                    if element.startswith('www.'):
+                        clean_list.append(element[4:])
+                    else:
+                        clean_list.append("www.%s" % element)
+                clean_list.append(element)
+
         clean_list = Helpers.List(clean_list).format()
-        whitelisted = Whitelist(string="\n".join(clean_list)).get()
+        whitelisted = clean_list_with_official_whitelist(clean_list)
 
         Helpers.File(Settings.clean_list_file).write(
             "\n".join(clean_list), overwrite=True
         )
 
-        Helpers.File(Settings.whitelisted_list_file).write(whitelisted, overwrite=True)
+        Helpers.File(Settings.whitelisted_list_file).write("\n".join(whitelisted), overwrite=True)
 
         Helpers.File("whitelisting.py").delete()
 
